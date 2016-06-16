68a10f7497ae83e725b0fe21865e6fd92f786a68
kenson-zhang <740183413@qq.com>
2016-06-16 12:57:12 +0800

    这是我测试的项目

---
+++ data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'thinkcmf',
+    'DB_USER' => 'root',
+    'DB_PWD' => '',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'cmf_',
+    //密钥
+    "AUTHCODE" => 'wnBJ0oeU444zagtl29',
+    //cookies
+    "COOKIE_PREFIX" => 'zKLXhm_',
+);
